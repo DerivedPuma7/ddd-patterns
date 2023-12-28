d91a521f9b89d1ace76f54bec8a201a0fd61416c
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-12-28 17:58:32 -0300

    setup de testes

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  clearMocks: true,
+  coverageProvider: "v8",
+  roots: [
+    "<rootDir>/tests"
+  ],
+};
+
+export default config;

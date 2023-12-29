2d130caffeda7e7459f6d406d50e50eea010fdfc
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-12-28 21:11:25 -0300

    torna atibutos verdadeiramente privados

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,43 +2,43 @@ import Address from "./address";
 
 
 export default class Customer {
-    _id: string;
-    _name: string;
-    _address!: Address
-    _active: boolean = false;
+    private id: string;
+    private name: string;
+    private address!: Address
+    private active: boolean = false;
 
     constructor(id: string, name: string) {
-        this._id = id;
-        this._name = name;
+        this.id = id;
+        this.name = name;
         this.validate();
     }
 
     validate() {
-        if(this._name.length === 0) {
+        if(this.name.length === 0) {
             throw new Error("Name is required");
         }
-        if(this._id.length === 0) {
+        if(this.id.length === 0) {
             throw new Error("Id is required");
         }
     }
 
     changeName(name: string) {
-        this._name = name;
+        this.name = name;
         this.validate();
     }
 
     activate() {
-        if(this._address === undefined) {
+        if(this.address === undefined) {
             throw new Error("Address is mandatory to activate a customer");
         }
-        this._active = true;
+        this.active = true;
     }
 
     deactivate() {
-        this._active = false;
+        this.active = false;
     }
 
     set Address(addres: Address) {
-        this._address = addres;
+        this.address = addres;
     }
 }

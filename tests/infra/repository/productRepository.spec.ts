1f9181f411d6a97960f785f50422a048ba6c32ea
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-03 19:46:31 -0300

    test: iniciando o test do repository

---
+++ tests/infra/repository/productRepository.spec.ts
@@ -0,0 +1,27 @@
+import ProductModel from "../../../src/infrastructure/db/sequelize/model/productModel"
+
+import { Sequelize } from "sequelize-typescript";
+
+describe("Product repository test", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false,
+            sync: { force: true },
+        });
+
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("", () => {
+        
+    });
+});

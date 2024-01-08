b7f7e7ee063e1aac58113455b69a71e0b66d2dba
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-08 20:14:50 -0300

    test: teste para o create do repositorio de Customer

---
+++ tests/infra/repository/customerRepository.spec.ts
@@ -0,0 +1,50 @@
+import CustomerModel from "../../../src/infrastructure/db/sequelize/model/customerModel";
+import CustomerRepository from "../../../src/infrastructure/repository/customerRepository";
+import Customer from "../../../src/domain/entity/customer";
+import Address from "../../../src/domain/entity/address";
+
+import { Sequelize } from "sequelize-typescript";
+
+describe("Customer repository test", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false,
+            sync: { force: true },
+        });
+
+        sequelize.addModels([CustomerModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a customer", async () => {
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer("1", "name");
+        const address = new Address("street 1", 1, "zip", "city");
+        customer.Address = address;
+        
+        await customerRepository.create(customer);
+
+        const customerModel = await CustomerModel.findOne({ where: { id: "1" } });
+        console.log(customerModel);
+
+        expect(customerModel?.toJSON()).toStrictEqual({
+            id: customer.id,
+            name: customer.name,
+            active: customer.isActive(),
+            rewardPoints: customer.rewardPoints,
+            street: address.street,
+            number: address.number,
+            zipcode: address.zip,
+            city: address.city,
+        });
+    });
+
+});

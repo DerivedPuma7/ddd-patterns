6cd383d5a61223c99d132f1fbcb041d8e10246e7
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-08 20:15:14 -0300

    feat: CustomerRepository.create()

---
+++ src/infrastructure/repository/customerRepository.ts
@@ -0,0 +1,27 @@
+import Customer from "../../domain/entity/customer";
+import CustomerRepositoryInterface from "../../domain/repository/customer-repository-interface";
+import CustomerModel from "../db/sequelize/model/customerModel";
+
+export default class CustomerRepository implements CustomerRepositoryInterface {
+    async create(entity: Customer): Promise<void> {
+        await CustomerModel.create({
+            id: entity.id,
+            name: entity.name,
+            street: entity.Address.street,
+            number: entity.Address.number,
+            zipcode: entity.Address.zip,
+            city: entity.Address.city,
+            active: entity.isActive(),
+            rewardPoints: entity.rewardPoints
+        });
+    }
+    async update(entity: Customer): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async find(id: string): Promise<Customer> {
+        throw new Error("Method not implemented.");
+    }
+    async findAll(): Promise<Customer[]> {
+        throw new Error("Method not implemented.");
+    }
+}

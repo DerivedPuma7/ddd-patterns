041b21eee65cb2b3150d4c8a62841c6b1e31a95d
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-18 20:08:33 -0300

    test: ajusta test do dispatcher

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -4,20 +4,11 @@ import EventDispatcher from "./eventDispatcher";
 describe("Domain events tests", () => {
     let sut: EventDispatcher;
 
-    beforeAll(() => {
+    beforeEach(() => {
         sut = new EventDispatcher();
     });
 
     it("should register an event handler", () => {
-        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
-
-        sut.register("ProductCreatedEvent", eventHandler);
-
-        expect(sut.getEventHandlers).toHaveBeenCalledWith("ProductCreatedEvent", eventHandler);
-        expect(sut.getEventHandlers).toHaveBeenCalledTimes(1);
-    });
-
-    it("should return handlers correctly", () => {
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
         sut.register("ProductCreatedEvent", eventHandler);
 

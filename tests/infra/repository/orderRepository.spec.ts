2700a3309c2e4d28ea7e051c0d977173528b2c32
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-19 18:43:30 -0300

    test: implementa testes do orderRepository

--- tests/infra/repository/orderRepository.spec.ts
+++ tests/infra/repository/orderRepository.spec.ts
@@ -87,4 +87,50 @@ describe("Order repository test", () => {
             ]
         });
     });
+
+    it("should find an order", async () => {
+		const customer = createDefaultCustomer();
+		const product = createDefaultProduct();
+		const orderItem1 = new OrderItem("1", "Order Item 1", product.price, product.id, 1);
+		const order = new Order("1", customer.id, [orderItem1]);
+		await customerRepository.create(customer);
+		await productRepository.create(product);
+		await orderRepository.create(order);
+
+		const orderFound = await orderRepository.find(order.id);
+
+		expect(orderFound).toEqual(order);
+	});
+
+    it("should throw when order is not found", async () => {
+        const fakeId = "any fake id";
+		expect(async () => {
+			await orderRepository.find(fakeId);
+		}).rejects.toThrow(`Order with id: ${fakeId} not found`);
+	});
+
+    it("should find all orders", async () => {
+		const customer = createDefaultCustomer();
+        const product1 = new Product("1", "fake product 1", 10);
+        const product2 = new Product("2", "fake product 2", 20);
+        const product3 = new Product("3", "fake product 3", 30);
+        const orderItem1 = new OrderItem("1", "fake order item 1", product1.price, product1.id, 1);
+        const orderItem2 = new OrderItem("2", "fake order item 2", product2.price, product2.id, 2);
+        const orderItem3 = new OrderItem("3", "Order Item 3", product3.price, product3.id, 3);
+		const order1 = new Order("329", customer.id, [orderItem1, orderItem2]);
+		const order2 = new Order("330", customer.id, [orderItem3]);
+
+		await customerRepository.create(customer);
+		await productRepository.create(product1);
+		await productRepository.create(product2);
+		await productRepository.create(product3);
+		await orderRepository.create(order1);
+		await orderRepository.create(order2);
+
+		const orders = await orderRepository.findAll();
+
+		expect(orders.length).toBe(2);
+		expect(orders).toContainEqual(order1);
+		expect(orders).toContainEqual(order2);
+	});
 });

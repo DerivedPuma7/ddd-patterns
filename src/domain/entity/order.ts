3b0056049eb8408f5aa957fd2159f15190cdb733
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-19 18:43:54 -0300

    feat: adiciona item na ordem

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -45,4 +45,8 @@ export default class Order {
     total(): number {
         return this._items.reduce((acc, item) => acc + item.total(), 0);
     }
+
+    addItem(orderItem: OrderItem): void {
+        this._items.push(orderItem);
+    }
 }

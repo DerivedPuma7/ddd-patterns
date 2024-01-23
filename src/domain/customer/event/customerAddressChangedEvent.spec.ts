7e1b9938a1911b76c89f4d63635fbae3bc6261ed
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-23 09:39:33 -0300

    test: verifica o parametro que o handler foi chamado

--- src/domain/customer/event/customerAddressChangedEvent.spec.ts
+++ src/domain/customer/event/customerAddressChangedEvent.spec.ts
@@ -7,9 +7,12 @@ describe("Customer changed address event tests", () => {
     it("should notify when a customer changes its address", () => {
         const eventDispatcher = new EventDispatcher();
         const customerAddressChangedEvent = new CustomerAddressChangedEvent({
-            id: 'any id',
-            name: 'any name',
-            address: new Address('any street', 123, 'any zip', 'any city')
+            dateTimeOccurred: new Date(),
+            data: {
+                id: 'any id',
+                name: 'any name',
+                address: new Address('any street', 123, 'any zip', 'any city')
+            }
         });
         const handler = new EnviaConsoleLogHandler();
         const eventHandlerSpy = jest.spyOn(handler, "handle");
@@ -18,5 +21,6 @@ describe("Customer changed address event tests", () => {
         eventDispatcher.notify(customerAddressChangedEvent);
 
         expect(eventHandlerSpy).toHaveBeenCalledTimes(1);
+        expect(eventHandlerSpy).toHaveBeenCalledWith(customerAddressChangedEvent);
     });
 });

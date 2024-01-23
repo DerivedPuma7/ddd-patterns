2c20a3d21917fe0c1621e28544371da76f645f8f
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-01-23 09:38:50 -0300

    refactor: melhora implementação do evento para facilitar os testes

--- src/domain/customer/event/customerAddressChangedEvent.ts
+++ src/domain/customer/event/customerAddressChangedEvent.ts
@@ -2,17 +2,20 @@ import Address from "../value-object/address";
 import EventInterface from "../../@shared/event/eventInterface";
 
 export type CustomerAddressChangedEventData = {
-    id: string,
-    name: string,
-    address: Address
+    dateTimeOccurred: Date,
+    data: {
+        id: string,
+        name: string,
+        address: Address
+    }
 }
 
 export default class CustomerAddressChangedEvent implements EventInterface {
     dataTimeOccurred: Date;
-    eventData: CustomerAddressChangedEventData;
+    eventData: any;
 
     constructor(eventData: CustomerAddressChangedEventData) {
-        this.dataTimeOccurred = new Date();
-        this.eventData = eventData;
+        this.dataTimeOccurred = eventData.dateTimeOccurred;
+        this.eventData = eventData.data;
     }
 }
